d2677699c6cb9f9e4d3c4f75f1fe97d809c9bd07
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-09-23 12:49:39 +0100

    Update program to check system resources

---
+++ resource_monitor.py
@@ -0,0 +1,12 @@
+import psutil, os
+
+THRESHOLD = 700 * 1024 * 1024
+
+def main():
+	while True:
+		mem_available = psutil.virtual_memory().available
+		if mem_available <= THRESHOLD:
+			os.system("spd-say 'System is running slow, close all applications'")
+
+if __name__ == "__main__":
+	main()
