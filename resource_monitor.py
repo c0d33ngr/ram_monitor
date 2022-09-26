a4f4ce05d5f54c49cd7044ef9bde605ba7fddf10
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-09-26 23:38:57 +0100

    Add comments to code for readability sake

--- resource_monitor.py
+++ resource_monitor.py
@@ -6,11 +6,15 @@ TITLE = "RAM Usage"
 MESSAGE = "RAM usage is too much... Close down some apps!!!"
 
 def show_app(app_name, title, message):
+	""" Function to display notification """
+	
 	notify2.init(app_name)
 	n = notify2.Notification(title, message)
 	n.show()
 	
 def main():
+	""" Starter function to run the program """
+	
 	mem_available = psutil.virtual_memory().available
 	if mem_available >= THRESHOLD:
 		show_app(APP_NAME, TITLE, MESSAGE)
